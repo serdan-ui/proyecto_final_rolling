fbdc66fd4a653fccba4e2745b2b1f045313f2550
rochyscarlata <rosarioscarlata24@gmail.com>
2020-05-14 16:43:42 -0300

    formando la main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import Carrousel from './Carrousel'
+import Container from './Container'
+import Fotter from '../Layout/Fotter'
+import Header from '../Layout/Header'
 // stilos de register
 import "./styles.css"
 
 const Main = () => {
-    return ( <h1>
-        desde Main
-    </h1> );
+    return ( 
+        <>
+        <Header/>
+        <Carrousel/>
+        <Container/>
+        <Fotter/>
+        </>
+     );
 }
  
 export default Main;
