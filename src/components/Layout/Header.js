b2978f57daa4e8ba9687f18c0e8996f679dfcf48
sergio <sergio_dr@hotmail.com.ar>
2020-05-15 14:45:15 -0300

    Header con nav responsive

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+import { Container, Row, Col, Nav, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 const Header = () => {
-    return (  <h1>he aqui header </h1>);
-}
- 
+  return (
+    <Container fluid className="Container_Header">
+      <Row className="container_logo">
+        <Col xs={3} className="logo">
+          logo
+        </Col>
+        <Col xs={9} className="login_registro_header">
+          <Button className="btnLogin_header"> Iniciar sesion</Button>
+          <Button className="btnCart_header"> Carrito</Button>
+        </Col>
+      </Row>
+      <Nav className="justify-content-center nav_header" >
+        <Nav.Item className="nav_header" >
+          <Link to="/shopping-cart" className="btnNav_header">Tienda</Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to="/service" className="btnNav_header">Turno</Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to="/main" className="btnNav_header">Consulta</Link>
+        </Nav.Item>
+      </Nav>
+    </Container>
+  );
+};
+
 export default Header;
