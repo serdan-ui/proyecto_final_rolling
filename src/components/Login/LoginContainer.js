a776575f48d8b24de9e12b1a3d5eee2e2c77a800
Silvia Granara <masil79@gmail.com>
2020-05-17 17:48:02 -0300

    traer cambios

--- src/components/Login/LoginContainer.js
+++ src/components/Login/LoginContainer.js
@@ -1,17 +1,46 @@
+
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Form, Button } from 'react-bootstrap';
 const LoginConteiner = () => {
-    return ( <Form>
-        <Form.Group>
-          <Form.Label>Ingrese su nombre de usuario</Form.Label>
-          <Form.Control placeholder= "username"/>
-        </Form.Group>
-        <Form.Group>
-        <Form.Label>Ingrese su contraseña</Form.Label>
-          <Form.Control type = "password" placeholder= "password"/>
-        </Form.Group>
-        <Button variants="primary" type="submit">Enviar</Button>
-      </Form> );
+
+const { register, errors, handleSubmit } = useForm();
+
+
+return (
+
+   <Form className="form-login">
+    <h3 className="text-center text-white">Iniciar sesion</h3>
+    <Form.Group>
+      <Form.Label>Ingrese su nombre de usuario</Form.Label>
+      <Form.Control
+       placeholder= "username"
+       ref={
+        register({
+            required: {
+                value: true,
+                message: 'Usuario es requerido. '
+            },
+            maxLength: {
+                value: 20,
+                message: 'No más de 20 carácteres!'
+            },
+            minLength: {
+                value: 5,
+                message: ' Usuario con mínimo 5 carácteres. '
+            }
+        })}
+      />
+    </Form.Group>
+    <Form.Group>
+    <Form.Label>Ingrese su contraseña</Form.Label>
+      <Form.Control 
+      type = "password"
+       placeholder= "password"
+       />
+    </Form.Group>
+    <Button variants="primary" type="submit">Enviar</Button>
+  </Form> );
 }
  
 export default LoginConteiner;
