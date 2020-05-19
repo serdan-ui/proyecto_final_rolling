80aad65be56e246ef77b010999169f2ae033e44a
Romero Nicole <luz.nicole.maria@gmail.com>
2020-05-19 20:25:52 -0300

    pppp

--- src/components/PageService/PageService.js
+++ src/components/PageService/PageService.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Container, Row, Col, CardColumns } from "react-bootstrap";
 
 // stilos de register
 import "./styles.css"
 
 const PageService = () => {
-    return ( <h2>
-        desde services
-    </h2> );
+    return ( 
+
+
+  <div className="container-fluid">
+    <div className="row">
+    <div className="row bg bg-danger">
+      <h4 className="text-center ">Nuestros servicios</h4>
+      <div className="somos col-6 bg bg-warning">hol chaaaau</div>
+      <div className="somos col-6 bg bg-warning">hahdgteghyfg</div>
+    </div>
+    <div className="row bg bg-primary">
+      
+        <div className="uno col-6 bg bg-dark">dbfgryfehe4uiejd</div>
+        <div className="uno col-6 bg bg-dark">sbdegyr</div>
+        <div className="uno col-6 bg bg-dark">hedhuygfyr</div>
+        <div className="uno col-6 bg bg-dark">hdbryr</div>
+      
+    </div>
+    </div>
+  </div>
+  
+
+        );
 }
  
 export default PageService;
