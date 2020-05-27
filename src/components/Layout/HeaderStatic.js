558bb450860a93d5086b93508b3ef4fd8ef9343a
Romero Nicole <luz.nicole.maria@gmail.com>
2020-05-27 07:56:47 -0300

    actualizo parte fotos

---
+++ src/components/Layout/HeaderStatic.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Container, Row, Col, Nav, Button, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./styles.css";
+import Loguito from '../Images/logosecundario.png'
+import {FaUser, FaCartPlus} from "react-icons/fa";
+
+
+const HeaderStatic = () => {
+
+
+  return (
+    <Container fluid className="Container_Header">
+      <Row className="container_logo">
+        <Col xs={4} md={3} className="logo">
+        <Image src={Loguito} rounded className="logo_imagen_header" />
+        </Col>
+        <Col xs={7} md={9} className="login_registro_header">
+          <Button className="btnLogin_header mr-2" href="http://localhost:3000/"> <FaUser className="icons_header"/>Iniciar sesion</Button>
+          
+        </Col>
+      </Row>
+
+      
+
+      <Nav className="justify-content-center nav_header" >
+        <Nav.Item className="nav_header" >
+          <Link to="/shopping-cart" className="btnNav_header">Tienda</Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to="/service" className="btnNav_header">Turno</Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to="/main" className="btnNav_header">Consulta</Link>
+        </Nav.Item>
+      </Nav>
+    </Container>
+  );
+};
+
+export default HeaderStatic;
--- src/components/PageService/PageService.js
+++ src/components/PageService/PageService.js
@@ -1,106 +1,34 @@
 import React from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import Avatar from "../Images/avatarprueba.png";
+import Rochy from "../Images/rochy.png";
+import Sergio from "../Images/sergio.png";
+import Nicole from "../Images/nicole1.jpeg";
+import Mariana from "../Images/mariana.png";
+import Silvia from "../Images/silvia.png";
+import Mauricio from "../Images/mauricio.png";
+import HeaderStatic from "../Layout/HeaderStatic";
 // stilos de register
 import "./styles.css";
 
 const PageService = () => {
-  return (
-    // <Container fluid className="padre">
-    //   <Row className="m-2">
-    //     <Col className="principal">
-    //       <h1 className="text-center titulo animate__heartBeat animate__delay-5s	5s"> Nuestros Servicios</h1>
-    //       <Row>
-    //         <Col responsive className="nuestroServ ">
-    //           <h4 > SOMOS SERVICIO OFICIAL de las principales marcas de audio y video.</h4>
-    //           <br></br>
-    //           <p> <strong>
-
-    //             Reparamos TV_LCD_LED - monitores-DVD-BLU-RAY-Camaras digitales -  <br></br>
-    //             filmadoras-Equipos de audio- minicomponentes-home cinema-
-    //               PLAY STATION (PS)- computadoras - microondas </strong></p>
-    //         </Col>
-
-    //         <Col className="nuestroServ">
-    //           <h4 className="text-center" >QUIENES SOMOS</h4>
-    //           <br></br>
-    //           <p> <strong>
-    //             Electronica Zero es un servicio técnico donde se realizan reparaciones
-    //              de productos electrónicos de todas las marcas,tenemos técnicos especializados. <br>
-    //             </br>
-    //           Trabajamos con repuestos originales y entregamos garantías escritas.
-    //            Financiamos con tarjetas, Recibimos débito y crédito. </strong></p>
-
-    //         </Col>
-    //       </Row>
-
-    //     </Col>
-
-    //     <Col className="principal">
-    //       <Row className="hijoServ">
-
-    //         <Col sm className="tipodeServ m-5"> <h4 className="titulo">Audio</h4>
-    //           <i class="ml-5 fas fa-volume-up fa-5x"></i>
-    //           <br></br>
-    //           <span>Reparamos equipos de audio con y sin garantía. Empleamos repuestos originales.</span>
-    //         </Col>
-    //         <Col sm className="tipodeServ m-5"> <h4 className="titulo">Tv Digital</h4>
-    //           <i class="ml-5 fas fa-tv fa-5x"></i>
-    //           <br></br>
-    //           <span>Se hacen reparaciones de tv led y lcd, monitores y computadores.</span>
-    //         </Col>
-    //       </Row>
-    //       <Row>
-    //         <Col sm className="tipodeServ m-5"> <h3 className="titulo">Responsabilidad</h3>
-    //           <i class="ml-5 far fa-clock fa-5x"></i> <br></br>
-    //           <span>Empleamos los mejores instrumentos de medición electrónica. Repuestos originales.</span>
-    //         </Col>
-    //         <Col sm className="tipodeServ m-5"> <h4 className="titulo">Soporte</h4>
-    //           <i class="ml-5 fas fa-phone fa-5x"></i>
-    //           <br></br>
-    //           <span>Ofrecemos garantía escrita de nuestros trabajos.</span>
-    //         </Col>
-    //       </Row>
-    //     </Col>
-
-    //   </Row>
-
-    //   <Row>
-    //     <Col className="persona m-2 roundedCircle"> <h3 className="text-center my-3"> persona 1</h3>
-
-    //     </Col>
-    //     <Col className="persona m-2 roundedCircle"> <h4 className="text-center my-3">persona 2</h4>
-
-    //     </Col>
-    //     <Col className="persona m-2"> <h4 className="text-center my-3">persona 3</h4>
-
-    //     </Col>
-    //     <Col className="persona m-2 roundedCircle"> <h4 className="text-center my-3">persona 4</h4>
-
-    //     </Col>
-    //     <Col className="persona m-2 roundedCircle"> <h4 className="text-center my-3">persona 5</h4>
-
-    //     </Col>
-    //     <Col className="persona m-2 roundedCircle"> <h4 className="text-center my-3">persona 6</h4>
-
-    //     </Col>
-    //   </Row>
-    // </Container>
-
-    <Container fluid>
+  return ( 
+    <>
+   <HeaderStatic/>
+    <Container fluid className="bodyPadre" >
       <Row>
         <Col xs={12} xl={6}>
           {" "}
-          <h1 className="text-center titulo animate__heartBeat animate__delay-5s	5s">
+          <h1 className="text-center textos m-3 text-white animate__heartBeat animate__delay-5s	5s">
             {" "}
-            Nuestros Servicios
+            NUESTROS SERVICIOS
           </h1>
           <Container fluid>
             {" "}
             <Row>
-              <Col xs={12} xl={6}>
+              <Col xs={12}  className="nuestroServ">
                 {" "}
-                <h4>
+                <h4 className="textos text-center">
                   {" "}
                   SOMOS SERVICIO OFICIAL de las principales marcas de audio y
                   video.
@@ -108,15 +36,31 @@ const PageService = () => {
                 <br></br>
                 <p>
                   {" "}
-                  <strong>
-                    Reparamos TV_LCD_LED - monitores-DVD-BLU-RAY-Camaras
-                    digitales - <br></br>
-                    filmadoras-Equipos de audio- minicomponentes-home cinema-
-                    PLAY STATION (PS)- computadoras - microondas{" "}
-                  </strong>
+                  
+                    Reparamos TV,LCD,LED , monitores,DVD,BLU,RAY,Camaras
+                    digitales
+                    filmadoras, Equipos de audio- minicomponentes,home cinema,
+                    PLAY STATION (PS),computadoras , microondas{" "}
+                  
+                </p>
+                <br></br>
+
+                <h3 className="text-center textos">QUIENES SOMOS</h3>
+                <br></br>
+                <p className="p-3">
+                  {" "}
+                  
+                    Electronica Zero es un servicio técnico donde se realizan
+                    reparaciones de productos electrónicos de todas las
+                    marcas,tenemos técnicos especializados.
+                    Trabajamos con repuestos originales y entregamos garantías
+                    escritas. Financiamos con tarjetas, Recibimos débito y
+                    crédito.{" "}
+                  
                 </p>
               </Col>{" "}
-              <Col>
+             
+              {/* <Col xs={12} xl={6} className="nuestroServ">
                 {" "}
                 <h4 className="text-center">QUIENES SOMOS</h4>
                 <br></br>
@@ -131,7 +75,7 @@ const PageService = () => {
                     crédito.{" "}
                   </strong>
                 </p>
-              </Col>
+              </Col> */}
             </Row>
           </Container>
         </Col>
@@ -140,9 +84,9 @@ const PageService = () => {
         <Col>
           <Container>
             <Row>
-              <Col xs={12} xl={6}>
+              <Col xs={12} xl={6} >
                 {" "}
-                <h4 className="titulo">Audio</h4>
+                <h4 className="titulo m-3">Audio</h4>
                 <i class="ml-5 fas fa-volume-up fa-5x"></i>
                 <br></br>
                 <span>
@@ -150,7 +94,7 @@ const PageService = () => {
                   repuestos originales.
                 </span>
               </Col>
-              <Col>
+              <Col className="m-3">
                 {" "}
                 <h4 className="titulo">Tv Digital</h4>
                 <i class="ml-5 fas fa-tv fa-5x"></i>
@@ -162,7 +106,7 @@ const PageService = () => {
               </Col>
             </Row>
             <Row>
-              <Col xs={12} xl={6}>
+              <Col xs={12} xl={6} >
                 {" "}
                 <h3 className="titulo">Responsabilidad</h3>
                 <i class="ml-5 far fa-clock fa-5x"></i> <br></br>
@@ -171,7 +115,7 @@ const PageService = () => {
                   Repuestos originales.
                 </span>
               </Col>
-              <Col>
+              <Col className="m-3">
                 {" "}
                 <h4 className="titulo">Soporte</h4>
                 <i class="ml-5 fas fa-phone fa-5x"></i>
@@ -182,12 +126,13 @@ const PageService = () => {
           </Container>
         </Col>
       </Row>
-      <Row className="justify-content-xl-center pt-5 contenedor-persona">
-        <Col xs={12} xl={2}>
+      <hr></hr>
+      <Row className="justify-content-xl-center contenedor-persona">
+        <Col xs={12} xl={2} className="contenedorPersona">
           {" "}
           <div className="persona roundedCircle " style={{ padding: "5px" }}>
             {" "}
-            <img style={{ height: "190px" }} src={Avatar} className="imagen-pers"/>{" "}
+            <img style={{ height: "190px" }} src={Rochy} className="imagen-pers"/>{" "}
             <div className="nombre-imagen">
               {" "}
               <p >Rosario Scarlata</p>
@@ -200,8 +145,13 @@ const PageService = () => {
         <Col xs={12} xl={2}>
           {" "}
           <div className="persona roundedCircle ">
+          
             {" "}
-            <img style={{ height: "190px" }} src={Avatar} />
+            <img style={{ height: "190px" }} src={Sergio} className="imagen-pers" />
+            <div className="nombre-imagen">
+              {" "}
+              <p >Sergio Rosales</p>
+            </div>
           </div>
         </Col>
         <Col xs={12} xl={2}>
@@ -210,8 +160,13 @@ const PageService = () => {
         <Col xs={12} xl={2}>
           {" "}
           <div className="persona roundedCircle ">
-            <img style={{ height: "190px" }} src={Avatar} />
+            <img style={{ height: "190px" }} src={Nicole} className="imagen-pers" />
+            <div className="nombre-imagen">
+              {" "}
+              <p >Nicole Romero</p>
+            </div>
           </div>
+
         </Col>
       </Row>
       <Row className="justify-content-xl-center pt-5 contenedor-persona">
@@ -221,7 +176,11 @@ const PageService = () => {
         <Col xs={12} xl={2}>
           {" "}
           <div className="persona roundedCircle ">
-            <img style={{ height: "190px" }} src={Avatar} />
+            <img style={{ height: "190px" }} src={Silvia} className="imagen-pers" />
+            <div className="nombre-imagen">
+              {" "}
+              <p > Silvia Granara</p>
+            </div>
           </div>
         </Col>
         <Col xs={12} xl={2}>
@@ -230,14 +189,47 @@ const PageService = () => {
         <Col xs={12} xl={2}>
           {" "}
           <div className="persona roundedCircle ">
-            <img style={{ height: "190px" }} src={Avatar} />
+            <img style={{ height: "190px" }} src={Mariana} className="imagen-pers" />
+            <div className="nombre-imagen">
+              {" "}
+              <p >Mariana Granara</p>
+            </div>
           </div>
         </Col>
         <Col xs={12} xl={2}>
           {" "}
         </Col>
       </Row>
+      <Row className="justify-content-xl-center pt-5 contenedor-persona">
+        <Col xs={12} xl={2}>
+         
+        </Col>
+        <Col xs={12} xl={2}>
+        
+        </Col>
+        <Col xs={12} xl={2}>
+          {" "}
+          <div className="persona roundedCircle ">
+          
+            {" "}
+            <img style={{ height: "190px" }} src={Mauricio} className="imagen-pers" />
+            <div className="nombre-imagen">
+              {" "}
+              <p >Mauicio Dranizareck</p>
+            </div>
+          </div>
+        </Col>
+        <Col xs={12} xl={2}>
+          {" "}
+        </Col>
+        <Col xs={12} xl={2}>
+        
+        
+
+        </Col>
+    </Row>
     </Container>
+    </>
   );
 };
 
