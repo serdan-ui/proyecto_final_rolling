7eb8059dac1cad7a3c6d2f24967da0f79e3a0a5c
rochyscarlata <rosarioscarlata24@gmail.com>
2020-05-31 20:52:44 -0300

    pusheando estilos

--- src/components/Layout/BtnCart.js
+++ src/components/Layout/BtnCart.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Button, ListGroup, Badge } from "react-bootstrap";
-import { FaCartPlus } from "react-icons/fa";
+import { Button, ListGroup, Badge, Row, Col } from "react-bootstrap";
+import { FaCartPlus, FaRegWindowClose } from "react-icons/fa";
 import { render } from "@testing-library/react";
 
 const BtnCart = (products) => {
@@ -29,14 +29,56 @@ const BtnCart = (products) => {
           <FaCartPlus className="icons_header_cart" />
           <Badge variant="dark">{products.products.length}</Badge>
         </Button>
-        <Button className="cartClose" onClick={cierroCarro}>
-          Cancelar
-        </Button>
+
+        <Row>
+          <Col sm={4}>
+            <Button className="cartClose" variant="dark" onClick={cierroCarro}>
+              <FaRegWindowClose />
+            </Button>
+          </Col>
+          <Col sm={8}>
+            <h4 style={{ color: "white" }}  className="cartClose">Productos seleccionados</h4>
+          </Col>
+        </Row>
         <ListGroup className="containerCart">
+          <Row>
+            <ListGroup horizontal className="estilosTitulosbtn">
+              <Col>
+                <ListGroup.Item className="estilosTitulosbtn">
+                  Imagen
+                </ListGroup.Item>
+              </Col>
+              <Col xs={6}>
+                <ListGroup.Item className="estilosTitulosbtn">
+                  Nombre y descripcion
+                </ListGroup.Item>
+              </Col>
+              <Col>
+                <ListGroup.Item className="estilosTitulosbtn">
+                  Precio
+                </ListGroup.Item>
+              </Col>
+            </ListGroup>
+          </Row>
+
           {products.products.lenght === 0
             ? null
             : products.products.map((product) => (
-                <ListGroup.Item>{product.nombre}</ListGroup.Item>
+                <Row>
+                  <ListGroup horizontal>
+                    <Col>
+                      <ListGroup.Item className="productitoslistados">{product.img}</ListGroup.Item>
+                    </Col>
+                    <Col xs={6}>
+                      <ListGroup.Item className="productitoslistados"> 
+                        {product.nombre} <br /> {product.descripcion}
+                      </ListGroup.Item>
+                    </Col>
+                    <Col>
+                      <ListGroup.Item className="productitoslistados"> ${product.precio}</ListGroup.Item>
+                    </Col>
+                  </ListGroup>
+                </Row>
               ))}
         </ListGroup>
       </div>
