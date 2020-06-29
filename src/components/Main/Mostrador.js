fdad851329436b512b48281e5ef66d1411b45fc2
rochyscarlata <rosarioscarlata24@gmail.com>
2020-06-29 04:37:04 -0300

    agregando solo el coso de filtro sin funcionalidad

--- src/components/Main/Mostrador.js
+++ src/components/Main/Mostrador.js
@@ -10,13 +10,15 @@ import {
   ListGroup,
   Alert,
   CardColumns,
-  Modal,
+  Dropdown,
+  Accordion,
 } from "react-bootstrap";
 import { FaCartPlus, FaWindowClose } from "react-icons/fa";
 import mousered from "../Images/mousered.png";
 
 import { CardText } from "react-bootstrap/Card";
 import swal from "sweetalert";
+
 
 const Mostrador = ({ setProducts, products }) => {
   // Estados
@@ -57,7 +59,40 @@ const Mostrador = ({ setProducts, products }) => {
       <p className="titulo_product_main">Productos</p>
       <Container fluid className="contenedor-mostrador">
         <Row style={{ background: "#171717" ,margin:"0px"}}>
-          <Col sm={2} style={{backgroundColor:'black'}} ></Col>
+          <Col sm={2} style={{backgroundColor:'black'}} > 
+          <h3 style={{color:"white"}}>Filtrar por: </h3>
+          <br/>
+          <Dropdown >
+  <Dropdown.Toggle  id="dropdown-basic" style={{backgroundColor:"#212121"}}>
+   Categoria
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item >Auriculares</Dropdown.Item>
+    <Dropdown.Item >Monitores</Dropdown.Item>
+    <Dropdown.Item >Mouse</Dropdown.Item>
+    <Dropdown.Item >Sillas</Dropdown.Item>
+    <Dropdown.Item >Teclados</Dropdown.Item>
+    <Dropdown.Item >Sillas</Dropdown.Item>
+
+  </Dropdown.Menu>
+</Dropdown>
+
+  <br/>
+  <br/>
+  <Dropdown>
+  <Dropdown.Toggle id="dropdown-basic" style={{backgroundColor:"#212121", outlineColor:"black"}}>
+    Precio
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Menor a mayor</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Mayor a menor</Dropdown.Item>
+   
+  </Dropdown.Menu>
+</Dropdown>
+
+  </Col>
           <Col  sm={8}  className="columnitax" style={{backgroundColor:'black'}}>
             <CardColumns className="cardColumns">
               {productos.map((producto) => (
