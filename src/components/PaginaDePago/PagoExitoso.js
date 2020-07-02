babaa9011d0d010120747f7315ef625038dd7db0
Romero Nicole <luz.nicole.maria@gmail.com>
2020-07-02 20:01:09 -0300

    edit y funcion en pagos y perfil

--- src/components/PaginaDePago/PagoExitoso.js
+++ src/components/PaginaDePago/PagoExitoso.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Button, Card, Col, Row, Container } from 'react-bootstrap'
+import {useHistory} from "react-router-dom"
 
 const PagoExitoso = () => {
+  let history = useHistory()
   return (<>
 
     <div className="container-fluid fondooexit">
@@ -13,19 +15,17 @@ const PagoExitoso = () => {
             <Col xl={12} lg={12} md={12} >
 
               <Card className="text-center m-5 cardmadreExit">
-                <Card.Header className="card-exit text-white"><h5>¡ Pago exitoso !</h5></Card.Header>
+                <Card.Header className="card-exit"><h5 className="text-white">¡ Pago exitoso !</h5></Card.Header>
 
-                <Card.Body>
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQHBCJMWW3a1baV8vzC1NaYFhhzmmbcpoK6jjfoj8SmIA&usqp=CAU&ec=45675148"
-                    style={{ height: '150px' }}></img>
-
-                 
+                <Card.Body className="imgeexit">
+                  <img src="https://thumbs.gfycat.com/ConstantThornyGalapagospenguin-small.gif"
+                    style={{ height: '200px' }}></img>
                   <Card.Text>
                     <h5>  <strong>¡Su pago fue procesado exitosamente!</strong></h5>
                   </Card.Text>
-                  <Button variant="primary">Volver</Button>
+                  <Button variant="primary" onClick={()=> history.push("/main")}>Volver</Button>
                 </Card.Body>
-                <Card.Footer className="card-exit text-white">2 days ago</Card.Footer>
+                <Card.Footer className="card-exit ">2 days ago</Card.Footer>
               </Card>
 
 
